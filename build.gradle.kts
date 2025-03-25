12673ff783e1534dc0685647b71dd2a8d85fb952
DavidChako <dchako@icosahedron.com>
2025-03-25 17:31:26 -0400

    sort transitive vulnerability in peer lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,9 +20,19 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
 
     implementation("ch.qos.logback:logback-classic:1.5.17")
+
     implementation("com.datomic:peer:1.0.7277")
+    implementation("org.clojure:clojure") {
+        // Fix transitive vulnerability:
+        // implementation("com.datomic:peer:1.0.7277")
+        // --> maven:org.clojure:clojure:1.10.0 is vulnerable
+        version {
+            strictly("1.12.0-alpha9")
+        }
+    }
 
     implementation("org.jetbrains.kotlin:kotlin-reflect:2.1.20")
+
 
     val spockVersion = "2.4-M5-groovy-4.0"
     testImplementation(platform("org.spockframework:spock-bom:$spockVersion"))
